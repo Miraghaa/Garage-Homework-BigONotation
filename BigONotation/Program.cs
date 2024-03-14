f2d638b183d34dcd6d79e8df0733e82a04a7b74b
Miraga99 <miraga041@gmail.com>
2024-03-14 17:32:45 +0400

    Add project files.

---
+++ BigONotation/Program.cs
@@ -0,0 +1,97 @@
+﻿// 1ci tapsiriq
+
+//int[] nums = { 1, 5, 7 };
+
+//void ArrayPlus(int[] arr, int num)
+//{
+//    Array.Resize(ref arr, arr.Length+1);
+//    arr[arr.Length-1] = num;
+
+//    Console.WriteLine(string.Join(", ",arr));
+//}
+//ArrayPlus(nums, 3);
+
+
+
+// 2ci tapsiriq
+
+//string name = "  Garage Academy   ";
+
+//string SpaceDelete(string worth)
+//{
+//    return worth.Trim();
+//}
+
+//Console.WriteLine(SpaceDelete(name));
+
+
+
+// 3cu tapsiriq
+
+
+//int[] minus = { 1, 3, 5, -7, -9 };
+
+//void Plus(int[] arr)
+//{
+//    for (int i = 0; i < arr.Length; i++)
+//    { 
+//        if (arr[i] < 0)
+//        {
+//            arr[i] *= -1;
+//        }
+//        Console.WriteLine(arr[i]);
+//    }
+//}
+//Plus(minus);
+
+
+
+// 4 cu tapsiriq
+
+//static int Factorial(int n)
+//{
+//    if (n == 0 || n == 1)
+//    {
+//        return 1; 
+//    }
+//    else
+//    {
+//        int factorial = 1;
+//        for (int i = 2; i <= n; i++)
+//        {
+//            factorial *= i;
+//        }
+//        return factorial;
+//    }
+//}
+//Console.WriteLine(Factorial(5));
+
+
+
+// 5ci tapsiriq
+
+//int Fibonacci(int n)
+//{
+//    if (n <= 1)
+//    {
+//        return n; 
+//    }
+//    else
+//    {
+//        int a = 0;
+//        int b = 1;
+//        int temp = 0;
+//        for (int i = 2; i <= n; i++)
+//        {
+//            temp = a + b;
+//            a = b;
+//            b = temp;
+//        }
+//        return b;
+//    }
+//}
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(Fibonacci(i));
+//}
